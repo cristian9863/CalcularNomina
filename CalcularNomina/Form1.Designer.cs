f47b8a5436c948a1393bfa333ad9112208075dcb
Cristian Rodriguez <cristian9863@yahoo.com>
2024-03-28 18:37:31 -0500

    Agregar archivos de proyecto.

---
+++ CalcularNomina/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace CalcularNomina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtIdentificacion = new System.Windows.Forms.TextBox();
+            this.txtAsignacionDia = new System.Windows.Forms.TextBox();
+            this.txtDiasLaborados = new System.Windows.Forms.TextBox();
+            this.btnAlmacenarRegistro = new System.Windows.Forms.Button();
+            this.btnCalcularSalario = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTotalDevengado = new System.Windows.Forms.TextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.error1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.error1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculo de la nomina del trabajador";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Identificacion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(93, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Asignacion Salario Dia";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(93, 277);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Dias Laborados";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(382, 93);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(167, 22);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtIdentificacion
+            // 
+            this.txtIdentificacion.Location = new System.Drawing.Point(382, 137);
+            this.txtIdentificacion.Name = "txtIdentificacion";
+            this.txtIdentificacion.Size = new System.Drawing.Size(167, 22);
+            this.txtIdentificacion.TabIndex = 6;
+            // 
+            // txtAsignacionDia
+            // 
+            this.txtAsignacionDia.Location = new System.Drawing.Point(382, 201);
+            this.txtAsignacionDia.Name = "txtAsignacionDia";
+            this.txtAsignacionDia.Size = new System.Drawing.Size(167, 22);
+            this.txtAsignacionDia.TabIndex = 7;
+            // 
+            // txtDiasLaborados
+            // 
+            this.txtDiasLaborados.Location = new System.Drawing.Point(382, 271);
+            this.txtDiasLaborados.Name = "txtDiasLaborados";
+            this.txtDiasLaborados.Size = new System.Drawing.Size(167, 22);
+            this.txtDiasLaborados.TabIndex = 8;
+            // 
+            // btnAlmacenarRegistro
+            // 
+            this.btnAlmacenarRegistro.Location = new System.Drawing.Point(96, 331);
+            this.btnAlmacenarRegistro.Name = "btnAlmacenarRegistro";
+            this.btnAlmacenarRegistro.Size = new System.Drawing.Size(167, 43);
+            this.btnAlmacenarRegistro.TabIndex = 9;
+            this.btnAlmacenarRegistro.Text = "Guardar el registro de empleado";
+            this.btnAlmacenarRegistro.UseVisualStyleBackColor = true;
+            this.btnAlmacenarRegistro.Click += new System.EventHandler(this.btnAlmacenarRegistro_Click);
+            // 
+            // btnCalcularSalario
+            // 
+            this.btnCalcularSalario.Location = new System.Drawing.Point(382, 322);
+            this.btnCalcularSalario.Name = "btnCalcularSalario";
+            this.btnCalcularSalario.Size = new System.Drawing.Size(118, 43);
+            this.btnCalcularSalario.TabIndex = 10;
+            this.btnCalcularSalario.Text = "Calcular Salario";
+            this.btnCalcularSalario.UseVisualStyleBackColor = true;
+            this.btnCalcularSalario.Click += new System.EventHandler(this.btnCalcularSalario_Click);
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Location = new System.Drawing.Point(621, 322);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(75, 43);
+            this.btnNuevo.TabIndex = 11;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(93, 407);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(148, 16);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Valor Total Devengado";
+            // 
+            // txtTotalDevengado
+            // 
+            this.txtTotalDevengado.Location = new System.Drawing.Point(382, 401);
+            this.txtTotalDevengado.Name = "txtTotalDevengado";
+            this.txtTotalDevengado.Size = new System.Drawing.Size(167, 22);
+            this.txtTotalDevengado.TabIndex = 13;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(621, 390);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 33);
+            this.btnSalir.TabIndex = 14;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // error1
+            // 
+            this.error1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.txtTotalDevengado);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.btnCalcularSalario);
+            this.Controls.Add(this.btnAlmacenarRegistro);
+            this.Controls.Add(this.txtDiasLaborados);
+            this.Controls.Add(this.txtAsignacionDia);
+            this.Controls.Add(this.txtIdentificacion);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.error1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtIdentificacion;
+        private System.Windows.Forms.TextBox txtAsignacionDia;
+        private System.Windows.Forms.TextBox txtDiasLaborados;
+        private System.Windows.Forms.Button btnAlmacenarRegistro;
+        private System.Windows.Forms.Button btnCalcularSalario;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtTotalDevengado;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ErrorProvider error1;
+    }
+}
+
---
+++ CalcularNomina/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcularNomina
+{
+    public partial class Form1 : Form
+    {
+       Empleado myEmpleado = new Empleado();
+        Nomina myNomina = new Nomina();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnAlmacenarRegistro_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text == "")
+            {
+                error1.SetError(txtNombre, "Debe ingresar un nombre");
+                txtNombre.Focus();
+                return;
+            }
+            error1.SetError(txtNombre, "");
+
+            //validar q solo deje ingresar numeros
+
+            decimal AsignacionDia;
+            if (!Decimal.TryParse(txtAsignacionDia.Text, out AsignacionDia))
+            {
+                error1.SetError(txtAsignacionDia, "Debe ingresar un numero");
+                txtNombre.Focus();
+                return;
+            }
+            error1.SetError(txtAsignacionDia, "");
+
+            myEmpleado.Nombre = txtNombre.Text;
+            myEmpleado.Identificacion = txtIdentificacion.Text;
+            myEmpleado.AsignacionDia = Convert.ToDecimal(txtAsignacionDia.Text);
+            myNomina.DiasLaborados = Convert.ToInt32(txtDiasLaborados.Text);
+            MessageBox.Show("Se guardo el registro correctamente");
+
+            
+        }
+
+        private void btnCalcularSalario_Click(object sender, EventArgs e)
+        {
+            txtTotalDevengado.Text = myNomina.CalcularNomina(Convert.ToInt32(myNomina.DiasLaborados), Convert.ToDecimal(myEmpleado.AsignacionDia)).ToString();
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            txtNombre.Clear();
+            txtIdentificacion.Clear();
+            txtAsignacionDia.Clear();
+            txtDiasLaborados.Clear();
+            txtTotalDevengado.Clear();
+        }
+    }
+}
